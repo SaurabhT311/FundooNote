0f2b9691c129629f33f4cf41c6327c7537837e41
SaurabhT311 <38470650+SaurabhT311@users.noreply.github.com>
2021-03-18 23:29:30 +0530

    [Saurabh]Add AddedSearchApiInCollaborator

--- User_Backend/Service/noteService.js
+++ User_Backend/Service/noteService.js
@@ -1,8 +1,8 @@
 const noteModels = require('../Model/noteModel');
 let { OK, NotFound, BadRequest } = require('../Middleware/httpStatusCode.json');
-const { callbackPromise } = require('nodemailer/lib/shared');
 const userModels = require('../Model/UserModel');
 const { error } = require('winston');
+
 
 class noteService {
 
@@ -68,7 +68,6 @@ class noteService {
     }
 
     addLabelToNoteService = (labelData, callback) => {
-
         noteModels.addLabelToNote(labelData, (err, result) => {
             if (err) {
                 return callback(err);
@@ -95,22 +94,80 @@ class noteService {
         return userModels.find(email)
             .then((result) => {
                 if (result) {
-                    console.log("r is:",result);
-                    let push = { $push: { collabId: result._id } }
-                    return noteModels.collabrationAdd_Remove(noteID, push)
+                    console.log("r is:", result);
+                    let push = { $push: { collabId: result._id }, new:true
+                }
+                    return noteModels.collabrationAddRemove(noteID, push)
                         .then((result) => {
-                            return ({ message: "Collabration Successfully", data: result, code:OK  });
+                            return ({ message: "Collabration Successfully", data: result, code: OK });
                         }).catch((err) => {
-                            return ({ message: "Collabration is Unsuccessful!!", error: err,  code:NotFound });
+                            return ({ message: "Collabration is Unsuccessful!!", error: err, code: NotFound });
                         });
                 } else {
-                    return ({ message: "User Not Found!!", error: err, code:NotFound });
+                    return ({ message: "User Not Found!!", error: err, code: NotFound });
                 }
             }).catch((err) => {
-                return ({ message: "Please Enter Correct User Credential", error: err, code:NotFound });
+                return ({ message: "Please Enter Correct User Credential", error: err, code: NotFound });
             });
     }
+
+
+//     removeCollaboratorService(noteId, email) {
+//         return userModels.search()
+//            .then((result) => {
+//                if (result) {
+//                    console.log("res is:", result);
+//                    let pull = { $pull: { collabId: result._id },new:true }
+//                    return noteModels.collabrationAddRemove(noteId, pull)
+//                        .then((result) => {
+//                            return ({ flag: true, message: "Removed Collaborator recipient", data: result, code: OK })
+//                        }).catch((err) => {
+//                            return ({ flag: false, message: "Removal of collaborator denied", error: err, code: NotFound })
+//                        })
+//                } else {
+//                    return ({ flag: false, message: "User Not Found", error: err, code: NotFound })
+//                }
+//            }).catch((err) => {
+//                return ({ flag: false, message: "Enter valid credentials", error: err, code: NotFound })
+//            })
+//    }
+
+    removeCollaboratorService(noteId, email) {
+         return userModels.find(email)
+            .then((result) => {
+                if (result) {
+                    console.log("res is:", result);
+                    let pull = { $pull: { collabId: result._id },new:true }
+                    return noteModels.collabrationAddRemove(noteId, pull)
+                        .then((result) => {
+                            return ({ flag: true, message: "Removed Collaborator recipient", data: result, code: OK })
+                        }).catch((err) => {
+                            return ({ flag: false, message: "Removal of collaborator denied", error: err, code: NotFound })
+                        })
+                } else {
+                    return ({ flag: false, message: "User Not Found", error: err, code: NotFound })
+                }
+            }).catch((err) => {
+                return ({ flag: false, message: "Enter valid credentials", error: err, code: NotFound })
+            })
     }
+
+    searchService(searchKey) {
+        console.log("search:",searchKey);
+        return userModels.search(searchKey)
+        
+            .then((result) => {
+                if (result.length == 0) {
+                    return ({ message: " Data Not Found!", error: err, code:NotFound });
+                } else {
+                    console.log("res is:",result);
+                    return ({ message: "Search Data Found", data: result, code:OK });
+                }
+            }).catch((err) => {
+                return ({ message: "Search Matching Data Not Found!", error: err, code:NotFound });
+            });
+    }
+}
 
 module.exports = new noteService();
 
